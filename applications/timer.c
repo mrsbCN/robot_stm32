4f57ef2ae72d1a672243f7de5e02f15317d934f5
mrsb <zgg807492312@gmail.com>
2019-07-31 16:35:52 +0800

    bug fixed

---
+++ applications/timer.c
@@ -0,0 +1,77 @@
+#include "timer.h"
+
+void timer_entry(void *par)
+{
+	while(1)
+	{
+		if(RT_EOK == rt_event_recv(&event_pwm, EVENT_PWM_LEFT|EVENT_PWM_RIGHT|EVENT_PWM_CAM, RT_EVENT_FLAG_OR | RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved))	//如果收到dist事件则开始计算
+        {
+			if(recved == EVENT_PWM_LEFT)
+			{
+				if(pulse_l == 500000)
+				{
+					rt_pin_write(SPEAKER1_PIN, PIN_LOW);
+					for(rt_uint8_t i =0;i<10;i++)
+					{
+						pulse_l += 200000;
+						rt_pwm_set(pwm_dev_left, PWM_LEFT, period, pulse_l);
+						rt_thread_mdelay(5);
+					}
+				}
+				else if(pulse_l == 2500000)
+				{
+					for(rt_uint8_t i =0;i<10;i++)
+					{
+						pulse_l -= 200000;
+						rt_pwm_set(pwm_dev_left, PWM_LEFT, period, pulse_l);
+						rt_thread_mdelay(5);
+					}
+				}
+			}
+			if(recved == EVENT_PWM_RIGHT)
+			{
+				if(pulse_r == 500000)
+				{
+					rt_pin_write(SPEAKER2_PIN, PIN_LOW);
+					for(rt_uint8_t i =0;i<10;i++)
+					{
+						pulse_l += 200000;
+						rt_pwm_set(pwm_dev_right, PWM_RIGHT, period, pulse_r);
+						rt_thread_mdelay(5);
+					}
+				}
+				else if(pulse_l == 2500000)
+				{
+					for(rt_uint8_t i =0;i<10;i++)
+					{
+						pulse_l -= 200000;
+						rt_pwm_set(pwm_dev_right, PWM_RIGHT, period, pulse_r);
+						rt_thread_mdelay(5);
+					}
+				}
+			}
+			
+		}
+		
+	}
+}
+
+void timer_init(void)
+{
+	period = 20000000;  //单位ns
+	pulse_l =  500000;	/* PWM脉冲宽度值，单位为纳秒ns */ //暂时假设0度是关闭
+	pulse_r =  500000;
+	pwm_dev_left = (struct rt_device_pwm *)rt_device_find(PWM_DEV_NAME);
+	rt_pwm_set(pwm_dev_left, PWM_LEFT, period, pulse_l);
+	rt_pwm_enable(pwm_dev_left, PWM_LEFT);
+	pwm_dev_right = (struct rt_device_pwm *)rt_device_find(PWM_DEV_NAME);
+	rt_pwm_set(pwm_dev_right, PWM_RIGHT, period, pulse_r);
+	rt_pwm_enable(pwm_dev_left, PWM_RIGHT);
+	
+	tid_timer = rt_thread_create("tid_timer",
+                                   timer_entry, RT_NULL,
+                                   2048 ,
+                                   8 , 10);		//优先级高一些
+    if(tid_timer != RT_NULL)
+        rt_thread_startup(tid_timer);
+}
